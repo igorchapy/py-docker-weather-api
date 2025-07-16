b3ed1ecf68def82eb8f687e9dd8d2e6580b5943a
Igor Chapy <misha@mate.acedemy>
2025-07-16 19:50:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,33 @@
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
 
+    if not api_key:
+        print("Ошибка: переменная окружения API_KEY не установлена.")
+        return
+
+    # параметры запроса
+    params = {
+        "key": api_key,
+        "q": CITY,
+        "aqi": "no"
+    }
+
+    try:
+        response = requests.get(URL, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        temp_c = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+
+        print(f"Current weather in {CITY}: ")
+        print(f"Temperature: {temp_c}°C")
+        print(f"Condition: {condition}")
+
+    except requests.exceptions.HTTPError as r_e:
+        print(f"HTTP Error: {r_e}")
+    except Exception as e:
+        print(f"Common Error: {e}")
 
 if __name__ == "__main__":
     get_weather()
